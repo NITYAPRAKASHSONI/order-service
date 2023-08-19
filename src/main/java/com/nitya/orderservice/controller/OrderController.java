dc09c1a74189595969e38b1c61ca12899ca56d9a
nityaprakashsoni <npsoni86051@gmail.com>
2023-08-19 09:08:32 +0530

    Initial commit

---
+++ src/main/java/com/nitya/orderservice/controller/OrderController.java
@@ -0,0 +1,23 @@
+package com.nitya.orderservice.controller;
+
+import com.nitya.orderservice.dto.OrderRequest;
+import com.nitya.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/order")
+public class OrderController {
+
+    private  final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest){
+        orderService.placeOrder(orderRequest);
+
+        return "Order Place Successfully";
+    }
+}
